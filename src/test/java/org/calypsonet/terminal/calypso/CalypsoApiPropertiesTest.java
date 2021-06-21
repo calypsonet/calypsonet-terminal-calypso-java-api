1de03daab0513bedb772673a7822f52da8ab91e8
Andrei Cristea <andrei.cristea019@gmail.com>
2021-06-21 11:38:34 +0200

    config: prepare release 1.0-rc1

--- src/test/java/org/calypsonet/terminal/calypso/CalypsoApiPropertiesTest.java
+++ src/test/java/org/calypsonet/terminal/calypso/CalypsoApiPropertiesTest.java
@@ -38,6 +38,7 @@ public class CalypsoApiPropertiesTest {
   @Test
   public void versionIsCorrectlyWritten() {
     String apiVersion = CalypsoApiProperties.VERSION;
-    assertThat(apiVersion).isEqualTo(libVersion).matches("\\d+\\.\\d+");
+    assertThat(apiVersion).matches("\\d+\\.\\d+");
+    assertThat(libVersion).startsWith(apiVersion);
   }
 }
