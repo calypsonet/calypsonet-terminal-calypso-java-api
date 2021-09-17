8d4b61c6e45182732bcdbc6acb06e1921ebc40d0
Andrei Cristea <andrei.cristea019@gmail.com>
2021-09-17 16:02:50 +0200

    docs: update javadoc publication

---
+++ .github/scripts/prepare_javadoc.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+echo "Compute the current API version..."
+
+version=$1
+
+if [ "$version" = "" ]
+then
+  version="`grep "^version" gradle.properties | cut -d= -f2 | tr -d "[:space:]"`-SNAPSHOT"
+fi
+
+echo "Computed current API version: $version"
+
+repository_name=`git rev-parse --show-toplevel | xargs basename`
+echo $repository_name
+
+echo "Clone $repository_name..."
+git clone https://github.com/calypsonet/$repository_name.git
+
+cd $repository_name
+
+echo "Checkout gh-pages branch..."
+git checkout -f gh-pages
+
+echo "Delete existing SNAPSHOT directory..."
+rm -rf *-SNAPSHOT
+
+echo "Delete existing RC directories in case of final release..."
+rm -rf $version-rc*
+
+echo "Create target directory $version..."
+mkdir $version
+
+echo "Copy javadoc and uml files..."
+cp -rf ../build/docs/javadoc/* $version/
+cp -rf ../src/main/uml/api_class_diagram.svg $version/
+
+echo "Update versions list..."
+echo "| Version | Documents |" > list_versions.md
+echo "|:---:|---|" >> list_versions.md
+for directory in `ls -rd */ | cut -f1 -d'/'`
+do
+  diagram=""
+  if [ -e $directory/api_class_diagram.svg ]
+  then
+    diagram=", [API class diagram]($directory/api_class_diagram.svg)"
+  fi
+  echo "| $directory | [API documentation]($directory)$diagram |" >> list_versions.md
+done
+
+echo "Computed all versions:"
+cat list_versions.md
+
+cd ..
+
+echo "Local docs update finished."
+
+
+
--- README.md
+++ README.md
@@ -1,19 +1,7 @@
-# 'Terminal Calypso API' Java implementation
+# Calypsonet Terminal Calypso Java API
 
 This is the repository for the **Calypso Networks Association**'s reference **Terminal Calypso API** for Java.
 
+API documentation & class diagram is available [here](https://calypsonet.github.io/calypsonet-terminal-calypso-java-api/).
+
 More information can be found on [calypsonet.org](http://calypsonet.org).
-
-## Terminal APIs
-
-This API is part of a set of three APIs.
-
-Java implementation:
-* [Terminal Calypso API](https://github.com/calypsonet/calypsonet-terminal-calypso-java-api)
-* [Terminal Card API](https://github.com/calypsonet/calypsonet-terminal-card-java-api)
-* [Terminal Reader API](https://github.com/calypsonet/calypsonet-terminal-reader-java-api)
-
-C++ implementation:
-* [Terminal Calypso API](https://github.com/calypsonet/calypsonet-terminal-calypso-cpp-api)
-* [Terminal Card API](https://github.com/calypsonet/calypsonet-terminal-card-cpp-api)
-* [Terminal Reader API](https://github.com/calypsonet/calypsonet-terminal-reader-cpp-api)
