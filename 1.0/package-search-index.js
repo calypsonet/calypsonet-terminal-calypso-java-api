23b7ef5329ecfe1b01546e6b4f8170fdbb0616f7
andrei-cristea <andrei.cristea019@gmail.com>
2021-09-17 15:49:18 +0200

    docs: delete 1.0 directory

--- 1.0/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.calypsonet.terminal.calypso"},{"l":"org.calypsonet.terminal.calypso.card"},{"l":"org.calypsonet.terminal.calypso.sam"},{"l":"org.calypsonet.terminal.calypso.transaction"}]
