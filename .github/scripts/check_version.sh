b85b0e89a469f0b4a672e9a39e76d040486a63f2
Andrei Cristea <andrei.cristea019@gmail.com>
2021-10-07 16:28:13 +0200

    ci: forbid the publication of a version already released
    Refs: #7

--- .github/scripts/check_version.sh
+++ .github/scripts/check_version.sh
@@ -5,19 +5,15 @@ version=`grep "^version" gradle.properties | cut -d= -f2 | tr -d "[:space:]"`
 
 if [ "$tag" != "" ]; then
   if [ "$tag" != "$version" ]; then
-    echo "ERROR: The tag '$tag' is different from the version '$version' in the 'gradle.properties' file."
+    echo "ERROR: the tag '$tag' is different from the version '$version' in the 'gradle.properties' file"
     exit 1
   fi
 fi
 
 git fetch --tags
 
-echo "1:$version"
-echo "2:`git tag -l "$version"`"
-echo "3:$(git tag -l "$version")"
-
 if [ $(git tag -l "$version") ]; then
-  echo "ERROR: Version '$version' has already been released."
+  echo "ERROR: version '$version' has already been released"
   exit 1
 fi
 
