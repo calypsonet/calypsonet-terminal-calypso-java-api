c313439ca587016b0de8f7a288b53e6b67837911
andrei-cristea <andrei.cristea019@gmail.com>
2022-01-04 17:36:25 +0100

    feat: Add "prepareUpdateBinary" and "prepareWriteBinary" methods to "CardTransactionManager" API (#31)
    Closes #19

--- src/main/java/org/calypsonet/terminal/calypso/CalypsoApiProperties.java
+++ src/main/java/org/calypsonet/terminal/calypso/CalypsoApiProperties.java
@@ -23,7 +23,7 @@ public class CalypsoApiProperties {
    *
    * @since 1.0.0
    */
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "1.1";
 
   /** Private constructor */
   private CalypsoApiProperties() {}
