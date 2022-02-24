34681b1cba851acd2a8ca296fcdc651ad9ad3ee5
andrei-cristea <andrei-cristea@users.noreply.github.com>
2022-02-24 14:28:47 +0000

    deploy: 27feaf3e040e88f5ffccaee5b4ba9102270c89fa

--- 1.2.0-SNAPSHOT/package-search-index.js
+++ 1.2.0-SNAPSHOT/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.calypsonet.terminal.calypso"},{"l":"org.calypsonet.terminal.calypso.card"},{"l":"org.calypsonet.terminal.calypso.sam"},{"l":"org.calypsonet.terminal.calypso.transaction"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.calypsonet.terminal.calypso"},{"l":"org.calypsonet.terminal.calypso.card"},{"l":"org.calypsonet.terminal.calypso.sam"},{"l":"org.calypsonet.terminal.calypso.spi"},{"l":"org.calypsonet.terminal.calypso.transaction"}]
